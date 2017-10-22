e354760f692f096c0d2ca1b9d153ce824aa7ca1e
Ray Matos <matos.ray@gmail.com>
2017-10-22 02:17:43 -0400

    edit

--- database.php
+++ database.php
@@ -1,7 +1,18 @@
 <?php
-$link = mysql_connect($_ENV["DB_HOST"], 'nextpost_user', 'E3LX3ZUqNgYXAPFC');
+echo $_ENV["DB_HOST"];
+
+
+$link = mysqli_connect($_ENV["DB_HOST"], 'nextpost_user', 'E3LX3ZUqNgYXAPFC');
+
 if (!$link) {
-    die('Could not connect: ' . mysql_error());
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
-printf("MySQL server version: %s\n", mysql_get_server_info());
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
 ?>
